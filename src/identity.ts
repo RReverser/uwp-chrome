8938f0f4022f9fdb250e8e8911cd70ada4dcf966
Ingvar Stepanyan <me@rreverser.com>
2016-02-15 22:06:37 +0000

    Add Google authentication & profile retrieval methods.
    To be used in future.

--- src/identity.ts
+++ src/identity.ts
@@ -1,7 +1,7 @@
 import { runtime, wrapAsync } from './runtime';
 
 const { WebAuthenticationBroker, WebAuthenticationOptions, WebAuthenticationStatus } = Windows.Security.Authentication.Web;
-const { HttpStatusCode, HttpClient, HttpFormUrlEncodedContent } = Windows.Web.Http;
+const { HttpStatusCode, HttpClient, HttpFormUrlEncodedContent, Headers: { HttpCredentialsHeaderValue } } = Windows.Web.Http;
 const { Uri } = Windows.Foundation;
 const { StringMap } = Windows.Foundation.Collections;
 
@@ -29,56 +29,92 @@ async function authenticate(interactive: boolean, start: Uri, end: Uri) {
 	}
 }
 
-const googleCallbackUrl = 'http://localhost/';
-const googleCallbackUri = new Uri(googleCallbackUrl);
+namespace Google {
+	const callbackUrl = 'http://localhost/';
+	const callbackUri = new Uri(callbackUrl);
+	const basicScopes = ['profile', 'email'];
+	const exchangeTokenUri = new Uri('https://www.googleapis.com/oauth2/v4/token');
+	const profileUri = new Uri('https://www.googleapis.com/plus/v1/people/me');
+	
+	export interface AuthResult {
+		access_token: string;
+		token_type: 'Bearer';
+		expires_in: number;
+		refresh_token?: string;
+		id_token?: string;
+	}
+	
+	export interface Profile {
+		id: string;
+		displayName: string;
+		emails: {
+			type: 'account' | 'home' | 'work' | 'other';
+			value: string;
+		}[]
+	}
 
-export const identity: typeof chrome.identity = {
-	getAuthToken: wrapAsync(async (details: chrome.identity.TokenDetails = {}) => {
-		let clientId = '';
+	export var userInfo = {
+		id: '',
+		email: ''
+	};
+
+	export async function auth(interactive: boolean, clientId: string, scopes: string[] = basicScopes) {
 		let url = await authenticate(
-			false,
+			interactive,
 			new Uri(`https://accounts.google.com/o/oauth2/auth?client_id=${
 				encodeURIComponent(clientId)
 			}&redirect_uri=${
-				encodeURIComponent(googleCallbackUrl)
+				encodeURIComponent(callbackUrl)
 			}&response_type=code&scope=${
-				encodeURIComponent(details.scopes.join(' '))
+				encodeURIComponent(scopes.join(' '))
 			}`),
-			googleCallbackUri
+			callbackUri
 		);
 		let code = new Uri(url).queryParsed.getFirstValueByName('code');
 		let content = new StringMap();
 		content.insert('code', code);
 		content.insert('client_id', clientId);
-		content.insert('redirect_uri', googleCallbackUrl);
+		content.insert('redirect_uri', callbackUrl);
 		content.insert('grant_type', 'authorization_code');
 		let http = new HttpClient();
 		try {
-			let response = await http.postAsync(new Uri('https://www.googleapis.com/oauth2/v4/token'), new HttpFormUrlEncodedContent(content));
+			let response = await http.postAsync(exchangeTokenUri, new HttpFormUrlEncodedContent(content));
 			try {
 				response.ensureSuccessStatusCode();
-				let {
-					access_token: accessToken,
-					refresh_token: refreshToken,
-					expires_in: expiresIn
-				}: {
-					access_token: string,
-					refresh_token: string,
-					expires_in: number
-				} = JSON.parse(await response.content.readAsStringAsync());
-				return accessToken;
+				return JSON.parse(await response.content.readAsStringAsync()) as AuthResult;
 			} finally {
 				response.close();
 			}
 		} finally {
 			http.close();
 		}
+	}
+	
+	export async function getProfile(authResult: AuthResult) {
+		let http = new HttpClient();
+		try {
+			http.defaultRequestHeaders.authorization = new HttpCredentialsHeaderValue(authResult.token_type, authResult.access_token);
+			let profile = JSON.parse(await http.getStringAsync(profileUri)) as Profile;
+			let accEmail = profile.emails.find(email => email.type === 'account');
+			userInfo = {
+				id: profile.id || '',
+				email: accEmail ? accEmail.value : ''
+			};
+			return profile;
+		} finally {
+			http.close();
+		}
+	}
+}
+
+export const identity: typeof chrome.identity = {
+	getAuthToken: wrapAsync(async (details: chrome.identity.TokenDetails = {}) => {
+		let { client_id, scopes } = runtime.getManifest().oauth2;
+		let authResult = await Google.auth(details.interactive, client_id, details.scopes || scopes);
+		return authResult.access_token;
 	}),
 
-	getProfileUserInfo: wrapAsync(async () => ({
-		email: '',
-		id: ''
-	})),
+	getProfileUserInfo: wrapAsync(() => Promise.resolve(Google.userInfo)),
 
 	launchWebAuthFlow: wrapAsync((details: chrome.identity.WebAuthFlowOptions) => authenticate(
 		details.interactive,
