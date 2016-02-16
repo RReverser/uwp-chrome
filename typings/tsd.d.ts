837ee213c15e9adc67aa2028a8d9c13388c3b8aa
Ingvar Stepanyan <me@rreverser.com>
2016-02-16 21:25:15 +0000

    Add typings to the repo due to various local fixes.

---
+++ typings/tsd.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="chrome/chrome-app.d.ts" />
+/// <reference path="chrome/chrome.d.ts" />
+/// <reference path="filesystem/filesystem.d.ts" />
+/// <reference path="filewriter/filewriter.d.ts" />
+/// <reference path="webrtc/MediaStream.d.ts" />
+/// <reference path="winrt/winrt-uwp.d.ts" />
